366c177f92960db0c19efc3ab1a1449576a02793
william <espiw@farmingdale.edu>
2024-02-26 18:46:44 -0500

    added @FXML fields for different objects

--- src/main/java/com/example/module5assignment/HelloController.java
+++ src/main/java/com/example/module5assignment/HelloController.java
@@ -2,6 +2,7 @@ package com.example.module5assignment;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class HelloController {
@@ -11,4 +12,13 @@ public class HelloController {
     private TextField textFieldLastName;
     @FXML
     private TextField textFieldDepartment;
+    @FXML
+    private TextField textFieldMajor;
+    @FXML
+    private TextField textFieldEmail;
+    @FXML
+    private TextField textFieldImageURL;
+    @FXML
+    private TableView tableViewStudentInfo;
+
 }
